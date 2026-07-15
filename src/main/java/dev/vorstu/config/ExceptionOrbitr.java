bbf03a5c4f360c3f5ea188f86ac54eef686260ef
ShadowFrog <vitalyarychagov@yandex.ru>
2026-07-15 17:55:02 +0400

    orbitr enable at properties

--- src/main/java/dev/vorstu/config/ExceptionOrbitr.java
+++ src/main/java/dev/vorstu/config/ExceptionOrbitr.java
@@ -2,14 +2,18 @@ package dev.vorstu.config;
 
 import dev.vorstu.exception.orbitr.OrbitrWantsAnException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Random;
 
 @Slf4j
 public class ExceptionOrbitr {
     private static final Random RANDOM = new Random();
+    @Value("${app.exception-orbitr.enable:false}")
+    private static boolean enable;
+
     public static void ask(){
-        if(RANDOM.nextBoolean()) {
+        if(enable&&RANDOM.nextBoolean()) {
             log.info("The great orbitr wants to throw an exception >_<");
             throw new OrbitrWantsAnException();
         }
